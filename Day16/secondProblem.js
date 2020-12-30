f7d771bcc5302af41d8e3a3944c778a2141a55cd
gulevskydev <d.gulevsky@mail.ru>
2020-12-30 22:25:35 +0300

    Day16

--- Day16/secondProblem.js
+++ Day16/secondProblem.js
@@ -2,10 +2,9 @@ function secondProblem(input) {
     const nums = {};
     const names = {}
     const data = {}
-
+    const ticketData = input[1].split(':')[1].split('\n')[1].split(',')
     input[0].split('\n').forEach((e,i) => names[i] = e.split(':')[0])
     input[0].split('\n').map(e => e.split(':')[1].split('or')).forEach((el,idx) => {
-        // console.log([el])
         el.forEach( e => {
             e = e.trim()
             const [f,l] = e.split('-').map(e => + e);
@@ -15,39 +14,30 @@ function secondProblem(input) {
             }
         })
     })
+
     input[2].split(':')[1].split('\n').slice(1).forEach((ar,i) => {
-        ar.split(',').forEach(el => {
+        ar.split(',').forEach((el,id) => {
             el = +el
-            if(!data[i]) data[i] = [el]
-            else data[i].push(el)
+            if(!data[id]) data[id] = [el]
+            else data[id].push(el)
         })
     })
 
-    const answers = []
+    ticketData.forEach((el,i) => {
+        data[i].push(+el)
+    })
 
-    for(let key in data) {
-        let idx
-
-        return Object.values(data).forEach(el => {
-            console.log(el.every(e=> Object.values(nums)[0].incldues(e))
-            // if(el.every(e => Object.values(nums).includes(e)){
-            //     return el
-            // }
-        })
-        for(let id in nums) {
-            if(nums[id].filter(el => data[key].includes(el)).length === data[key].length){
-                idx = id
-                console.log(data[key])
-                console.log(nums[id])
+    const answers = {}
+    const numsArray = Object.values(nums)
+    const dataArray = Object.values(data)
+    const tickets =  input[1].split(':')[1].split('\n')[1].split(',')
+    for(let i = 0; i < dataArray.length; i++) {
+        for(let j = 0; j < numsArray.length; j++) {
+            if (dataArray[i].filter(el => numsArray[j].includes(el)).length === dataArray[i].length) {
+                answers[names[j]] = tickets[i]
             }
         }
-        const values =  input[1].split(':')[1].split('\n')[1].split(',')
-        answers.push([names[idx],values[idx]])
-        // return res
-        // return nums[key].find(el => data[key].every(num => nums[key].includes(num))
     }
 
-    return answers
-
-    // return input[2].split(':')[1].split('\n').join(',').split(',').slice(1).filter(e => !nums.includes(+e)).reduce((a,b) => +a + +b)
+    return Object.entries(answers).filter(el => el[0].startsWith('departure')).reduce((a,b) => a * +b[1], 1)
 }
