9e11f5321dc042ef3a55e42e30745c82e9db0559
gulevskydev <d.gulevsky@mail.ru>
2021-01-01 15:02:34 +0300

    Day22

--- Day22/secondProblem.js
+++ Day22/secondProblem.js
@@ -1,4 +1,4 @@
-function recursiveCombat(p1, p2) {
+function secondProblem(p1, p2) {
     const store = new Set();
     while(p1.length > 0 && p2.length > 0) {
         let winner;
@@ -9,7 +9,7 @@ function recursiveCombat(p1, p2) {
         const card1 = p1.shift(), card2 = p2.shift();
         store.add(hash);
         if(p1.length >= card1 && p2.length >= card2) {
-            const {winner: player } = recursiveCombat(p1.slice(0, card1), p2.slice(0, card2));
+            const {winner: player } = secondProblem(p1.slice(0, card1), p2.slice(0, card2));
             winner = player;
         } else {
             winner = card1 > card2 ? 1 : 2;
@@ -22,5 +22,5 @@ function recursiveCombat(p1, p2) {
         res: p1.length > 0 ? p1 : p2
     };
 }
-const {res} = recursiveCombat([...s1], [...s2]);
+const {res} = secondProblem([...s1], [...s2]);
 return res.reduce((a,c,i) => a + (c * (res.length - i)), 0);
